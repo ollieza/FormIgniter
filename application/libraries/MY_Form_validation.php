b47827a1dbf160c86f4aa7f5c8866752df551478
Ollie Rattue <orattue@toomanytabs.com>
2010-11-06 13:05:01 +0100

    Fixed spacing

--- application/libraries/MY_Form_validation.php
+++ application/libraries/MY_Form_validation.php
@@ -1,37 +1,41 @@
 <?php
-class MY_Form_validation extends CI_Form_validation {
 
-        function MY_Form_validation()
-        {
-			parent::CI_Form_validation();
-        }
-        
-        // --------------------------------------------------------------------
-        /**
-       	 * Required if another field has a value (related fields)
-       	 *
-       	 * @access	public
-       	 * @param	string
-       	 * @return	bool
-       	 */
-       	 
-        function requiredif($str, $field)
-       	{
-	        if ($_POST[$field] == '')
-			{
-				return TRUE;	// the related form is blank			
-			}
-			
-			// the related form is set proceed with normal required
-			
-			if ( ! is_array($str)) 
-     		{
-     			return (trim($str) == '') ? FALSE : TRUE;
-     		}
-     		else
-     		{
-     			return ( ! empty($str));
-     		}
-        }
+class MY_Form_validation extends CI_Form_validation 
+{
+	function MY_Form_validation()
+	{
+		parent::CI_Form_validation();
+	}
+	
+	// --------------------------------------------------------------------
+	
+	/**
+	* Required if another field has a value (related fields)
+	*
+	* @access	public
+	* @param	string
+	* @return	bool
+	*/
+	
+	function requiredif($str, $field)
+	{
+		if ($_POST[$field] == '')
+		{
+			return TRUE; // the related form is blank			
+		}
+		
+		// the related form is set proceed with normal required
+		
+		if (!is_array($str))
+		{
+			return (trim($str) == '') ? FALSE : TRUE;
+    	}
+		else
+		{
+			return ( ! empty($str));
+    	}
+	}
+	
+	// --------------------------------------------------------------------
 }
 ?>
